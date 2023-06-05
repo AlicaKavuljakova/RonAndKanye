b53daae12123027a324b9830edbaa9b752ac4bda
alica kavuljakova <drobeek@gmail.com>
2023-06-05 14:43:05 -0700

    api

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RonAndKanye;
+
+QuoteGenerator.KanyeQuote();
+QuoteGenerator.RonQuote();
+QuoteGenerator.KanyeQuote();
+QuoteGenerator.RonQuote();
+
+QuoteGenerator.KanyeQuote();
+QuoteGenerator.RonQuote();
+
